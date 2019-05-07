81d431cbfd5ae47a06b339c60c615cd088898f64
Aquatic Raphael <37901809+AquaticRaphael@users.noreply.github.com>
2019-05-07 20:10:44 +0300

    Desktop based offline dictionary app.

---
+++ dictionary.py
@@ -0,0 +1,35 @@
+"""
+This is the very first program I have followed through in a tutorial and 
+managed to write from start to finish without errors. 
+I tried it more than once to make to work. I am very proud it did.
+"""
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def translate(w):
+	w = w.lower()#Helps ignore the user's punctuation of first word.
+	if w in data:
+		return data[w]
+	elif len(get_close_matches(w, data.keys())) > 0:
+		yn = input("Did you mean %s instead? Reply with Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+		yn = yn.upper()
+		if yn == "Y":
+			return data[get_close_matches(w, data.keys())[0]]
+		elif yn == "N":
+			return "The word does not exist. Please double check."
+		else:
+			return "We did not understand your reply."
+	else:
+		return "The word does not exist. Please double check."
+	pass
+
+word = input("Enter your word: ")
+output = translate(word)
+
+if type(output) == list:
+	for item in output:
+		print(item)
+else:
+	print(output)
